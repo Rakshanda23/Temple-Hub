af3fffcd6dba023630b41a0fad46ebc0c59b1bd9
Shreya Agrawal <shreyaparasagrawal@gmail.com>
2021-09-03 23:27:33 +0530

    Card done!

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +12,14 @@
   <p>Hello WOrld..!!</p>
   <p> trying to pull</p>
   <h1>Rakshanda here</h1>
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="dol.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
   </body>
 <!-- </body> -->
 </html>
